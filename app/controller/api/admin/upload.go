74f9266bfea942e0c1d0787e2e1d0f1897ec216f
daifuyang <1140444693@qq.com>
2020-06-03 20:40:34 +0800

    merge add

--- app/controller/api/admin/upload.go
+++ app/controller/api/admin/upload.go
@@ -1,10 +1,14 @@
 package admin
 
 import (
+	"encoding/json"
 	"fmt"
+	"gincmf/app/model"
 	"gincmf/app/util"
 	"github.com/gin-gonic/gin"
+	cmf "github.com/gincmf/cmf/bootstrap"
 	"github.com/gincmf/cmf/controller"
+	"strconv"
 )
 
 // AssetsController 图片资源控制器，定义了资源文件增删改查接口
@@ -36,15 +40,77 @@ func (rest *UploadController) Store(c *gin.Context) {
 
 	maxFiles := c.PostForm("max_files")
 	chunkSize := c.PostForm("chunk_size")
-	ImageMaxFileSize := c.PostForm("file_types[image][upload_max_file_size]")
-	ImageExtensions := c.PostForm("file_types[image][extensions]")
+	imageMaxFileSize := c.PostForm("file_types[image][upload_max_file_size]")
+	imageExtensions := c.PostForm("file_types[image][extensions]")
 
-	fmt.Println("maxFiles",maxFiles)
-	fmt.Println("chunkSize",chunkSize)
-	fmt.Println("ImageMaxFileSize",ImageMaxFileSize)
-	fmt.Println("ImageExtensions",ImageExtensions)
+	videoMaxFileSize := c.PostForm("file_types[video][upload_max_file_size]")
+	videoExtensions := c.PostForm("file_types[video][extensions]")
 
-	rest.rc.Success(c, "修改成功", nil)
+	audioMaxFileSize := c.PostForm("file_types[audio][upload_max_file_size]")
+	audioExtensions := c.PostForm("file_types[audio][extensions]")
+
+	fileMaxFileSize := c.PostForm("file_types[file][upload_max_file_size]")
+	fileExtensions := c.PostForm("file_types[file][extensions]")
+
+	maxFilesInt ,err := strconv.Atoi(maxFiles)
+	if err != nil {
+		rest.rc.Error(c,"最大同时上传文件数必须为数字",nil)
+		return
+	}
+	chunkSizeInt, err := strconv.Atoi(chunkSize)
+	if err != nil {
+		rest.rc.Error(c,"文件分块上传分块大小必须为数字",nil)
+		return
+	}
+	imageMaxFileSizeInt ,err := strconv.Atoi(imageMaxFileSize)
+	if err != nil {
+		rest.rc.Error(c,"允许图片上传大小必须为数字",nil)
+		return
+	}
+	videoMaxFileSizeInt ,err := strconv.Atoi(videoMaxFileSize)
+	if err != nil {
+		rest.rc.Error(c,"允许视频上传大小必须为数字",nil)
+		return
+	}
+	audioMaxFileSizeInt ,err := strconv.Atoi(audioMaxFileSize)
+	if err != nil {
+		rest.rc.Error(c,"允许音频上传大小必须为数字",nil)
+		return
+	}
+	fileMaxFileSizeInt ,err := strconv.Atoi(fileMaxFileSize)
+	if err != nil {
+		rest.rc.Error(c,"允许音频上传大小必须为数字",nil)
+		return
+	}
+
+	uploadSetting := &model.UploadSetting{
+		MaxFiles:  maxFilesInt,
+		ChunkSize: chunkSizeInt,
+		FileTypes: model.FileTypes{
+			Image: model.TypeValues{
+				UploadMaxFileSize: imageMaxFileSizeInt,
+				Extensions:        imageExtensions,
+			},
+			Video: model.TypeValues{
+				UploadMaxFileSize: videoMaxFileSizeInt,
+				Extensions:        videoExtensions,
+			},
+			Audio: model.TypeValues{
+				UploadMaxFileSize: audioMaxFileSizeInt,
+				Extensions:        audioExtensions,
+			},
+			File: model.TypeValues{
+				UploadMaxFileSize: fileMaxFileSizeInt,
+				Extensions:        fileExtensions,
+			},
+		},
+	}
+	uploadSettingValue, _ := json.Marshal(uploadSetting)
+	fmt.Println("uploadSettingValue", string(uploadSettingValue))
+
+	cmf.Db.Model(&model.Option{}).Where("option_name = ?","upload_setting").Update("option_value", string(uploadSettingValue))
+
+	rest.rc.Success(c, "修改成功",uploadSetting)
 }
 
 func (rest *UploadController) Delete(c *gin.Context) {
